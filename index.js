e975c2e8528b68a095f28b9c730e401cbd90c04c
leticiaxavier <50848766+LeticiaXavier@users.noreply.github.com>
2024-09-19 00:55:41 -0300

    Menu
    Foi adicionado o meu da aplicação

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const start = () => { 
-    
-    while (true){
-        let opcao = "Cadastrar";
+const { select } = require('@inquirer/prompts');
 
-        switch (opcao){
+
+const start = async () => {
+
+    while (true) {
+
+        const opcao = await select({
+            message: 'Menu >',
+            choices: [{
+                name: 'Cadastrar meta',
+                value: 'Cadastrar'
+            },
+            {
+                name: 'Sair',
+                value: 'Sair'
+            }
+        ], 
+        })
+
+        switch (opcao) {
             case "Cadastrar":
                 console.log(" Vamos cadastrar");
                 break;
-            
+
             case "Listar":
                 console.log(" Vamos listar");
                 break;
-            
+
             case "Sair":
+                console.log("Ate a proxima!");
                 return;
         }
     }
 
 }
+start(); // Chama a função para iniciar o menu
